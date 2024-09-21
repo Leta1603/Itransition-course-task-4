ab3b6008a9ecca35f0fe423a20eb1f905a0866ee
Leta1603 <dawa1603@mail.ru>
2024-09-21 22:12:31 +0300

    Create get and post methods in server

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mysql from "mysql";
+
+const app = express();
+
+const PORT = 8800;
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Violetta",
+  database: "itransition",
+});
+
+app.use(express.json());
+
+app.listen(PORT, (err) => {
+  err ? console.log(err) : console.log(`Listening port ${PORT}`);
+});
+
+app.get("/users", (req, res) => {
+  const query = "SELECT * FROM users";
+  db.query(query, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/users", (req, res) => {
+  const query = "INSERT INTO users (`name`, `email`) VALUES (?)";
+  const values = [req.body.name, req.body.email];
+  db.query(query, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
